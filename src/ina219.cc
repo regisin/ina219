6504f66bd358c7b26bf7348742f3f7024749c097
Paulo Alexandre Regis <regisin@gmail.com>
2018-03-13 11:03:47 -0700

    fix typo

--- src/ina219.cc
+++ src/ina219.cc
@@ -106,7 +106,7 @@ INA219::determine_current_lsb(float max_expected_amps, float max_possible_amps)
 	float nearest = roundf(max_possible_amps * 1000.0) / 1000.0;
 	if (max_expected_amps > nearest) {
 		char buffer[65];
-		sprintf(buffer, "Expected current %d A is greater than max possible current %d A", max_expected_amps, max_possible_amps);
+		sprintf(buffer, "Expected current %f A is greater than max possible current %f A", max_expected_amps, max_possible_amps);
 		perror(buffer);
 	}
 
